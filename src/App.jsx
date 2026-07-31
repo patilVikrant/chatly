3c571ca129496648162cb219498329b6f129f7c4
Vikrant Patil <pvikrant248@gmail.com>
2026-07-31 13:40:21 +0530

    Add React Router with placeholder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.css";
 
 function App() {
-  return <div>Chatly</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
