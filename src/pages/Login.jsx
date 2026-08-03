3f5760b780eadd11f06e08e06fefa692f45aff60
Vikrant Patil <pvikrant248@gmail.com>
2026-08-03 17:55:50 +0530

    feat: add Login page with login API integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,85 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Auth.css";
+import { useAuth } from "../context/AuthContext";
+import api from "../services/api";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { setUser, setToken } = useAuth();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData((prevValue) => ({
+      ...prevValue,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      const res = await api.post("/auth/login", formData);
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+
+      setToken(res.data.token);
+      setUser(res.data.user);
+
+      navigate("/");
+    } catch (err) {
+      setError(
+        err.response?.data?.message ||
+          "Something went wrong. Please try again.",
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="auth-card">
+        <h1 className="auth-title">Chatly</h1>
+        <p className="auth-subtitle">Login into your account</p>
+        <form onSubmit={handleSubmit} className="auth-form">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          {error && <p className="auth-error">{error}</p>}
+          <button type="submit" disabled={loading} className="auth-button">
+            {loading ? "Logging In..." : "Login"}
+          </button>
+        </form>
+        <p className="auth-footer">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
