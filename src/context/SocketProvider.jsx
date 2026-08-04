345b65f57bfcd6da672d7b2b8222bea7dcc76af5
Vikrant Patil <pvikrant248@gmail.com>
2026-08-05 01:37:17 +0530

    feat: add SocketContext for real-time connection

---
+++ src/context/SocketProvider.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
+import { io } from "socket.io-client";
+import { SocketContext } from "./SocketContext";
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const { token } = useAuth();
+
+  useEffect(() => {
+    if (!token) return;
+
+    const newSocket = io("https://chatly-backend-one.vercel.app", {
+      auth: {
+        token: token,
+      },
+    });
+
+    newSocket.on("connect", () => {
+      console.log("Socket connected:", newSocket.id);
+      setSocket(newSocket);
+    });
+
+    return () => {
+      newSocket.disconnect();
+      setSocket(null);
+    };
+  }, [token]);
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import { SocketProvider } from "./context/SocketProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </AuthProvider>
   </StrictMode>,
 );
